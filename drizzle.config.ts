faf04b49ca82a2a56ef50cb1d9b45a331177604d
Anish Kumar <kanish92729@gmail.com>
2024-06-21 08:28:45 +0530

    first commit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ export default defineConfig({
   schema: "./src/db/schema.ts",
   driver: "pg",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    connectionString: "postgresql://neondb_owner:bUP07hfLJywX@ep-bitter-wind-a1yy50d4.ap-southeast-1.aws.neon.tech/monty?sslmode=require",
   },
   verbose: true,
   strict: true,
